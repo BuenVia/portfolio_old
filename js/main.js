f90cdf55e89e0994a295a1ecdfdcf51dfe533843
SaracensFan <matthewclifford@hotmail.co.uk>
2021-03-31 08:34:31 +0100

    rework of portfolio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function calculator() {
-    let value1 = ;
-    let value2 = ;
-    let operator = ;
-    
-    if (operator == +) {
-        document.getElementById()
+function openCv() {
+    let cv = document.getElementById('summary');
+
+    if (cv.className === 'sidebar') {
+        cv.className += ' change';
+    } else {
+        cv.className = 'sidebar';
     }
 }
