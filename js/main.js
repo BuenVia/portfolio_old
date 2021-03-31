8589f79ab7d77602571fb4e007a6b2ed31feb5ea
SaracensFan <matthewclifford@hotmail.co.uk>
2021-03-31 19:11:14 +0100

    latest updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Function for opening Navbar (Hamburger element)
 function navOpen() {
     let nav = document.getElementById('navhead');
     if (nav.className === 'header') {
@@ -7,27 +8,31 @@ function navOpen() {
     }
 }
 
+// Portfolio modal
 let pModal = document.getElementById('portfolioMod');
 let pBtn = document.getElementById('portfolioBtn');
-let conModal = document.getElementById('contactMod');
-let conBtn = document.getElementById('contactBtn');
 let portClose = document.getElementsByClassName('portClose')[0];
-let conClose = document.getElementsByClassName('conClose')[0];
 
 pBtn.onclick = function() {
     pModal.style.display = "block";
 }
-conBtn.onclick = function() {
-    conModal.style.display = "block"
-}
 portClose.onclick = function() {
     pModal.style.display = "none";
-    conModal.style.display = "none";
+}
+
+// Contact form modal (sidebar)
+let conModal = document.getElementById('contactMod');
+let conBtn = document.getElementById('contactBtn');
+let conClose = document.getElementsByClassName('conClose')[0];
+
+conBtn.onclick = function() {
+    conModal.style.display = "block"
 }
 conClose.onclick = function() {
     conModal.style.display = "none";
 }
 
+// Closing all modals
 window.onclick = function(event) {
     if(event.target == pModal || event.target == conModal) {
         pModal.style.display = "none";
