f15ad8c381f4a2288792a181dcd22dd733d17ba6
SaracensFan <matthewclifford@hotmail.co.uk>
2021-03-31 10:53:52 +0100

    include js navbar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function navOpen() {
+    let nav = document.getElementById('navhead');
+    if (nav.className === 'header') {
+        nav.className += ' change';
+    } else {
+        nav.className = 'header';
+    }
+}
