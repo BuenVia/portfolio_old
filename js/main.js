b383233e5fa576af89c12ecaf59bedca2b4561a3
SaracensFan <matthewclifford@hotmail.co.uk>
2021-03-31 10:25:56 +0100

    design update

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function openCv() {
-    let cv = document.getElementById('summary');
-
-    if (cv.className === 'sidebar') {
-        cv.className += ' change';
-    } else {
-        cv.className = 'sidebar';
-    }
-}
